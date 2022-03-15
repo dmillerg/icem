8e37ff47824a8cbcf3ab9c908f8f65efe3bd583f
kuroko <dmillergg@gmail.com>
2022-03-15 01:34:40 +0100

    ajustes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,49 @@
-import { Component, OnInit } from '@angular/core';
+import { animate, query, stagger, style, transition, trigger } from '@angular/animations';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 
+const listAnimation = trigger('listAnimation', [
+  transition('* <=> *', [
+    query(':enter',
+      [style({ transform: 'translateX(50%)', opacity: 0 }), stagger('100ms', animate('1000ms ease-out', style({ transform: 'translateX(0%)', opacity: 1 })))],
+      { optional: true }
+    ),
+    query(':leave',
+      animate('200ms', style({ opacity: 0 })),
+      { optional: true }
+    )
+  ])
+]);
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
+  animations: [listAnimation,
+    trigger('scaleAnimation', [
+      transition(':enter', [
+        style({ transform: 'translateX(-50%)', opacity: 0 }),
+        animate('500ms', style({ transform: 'translateX(0%)', opacity: 1 })),
+      ]),
+      transition(':leave', [
+        style({ transform: 'scale(1)', opacity: 1 }),
+        animate('500ms', style({ transform: 'scale(0)', opacity: 0 })),
+      ]),
+    ]),
+  ],
 })
 export class AppComponent implements OnInit {
+  
   title = 'ICEM';
   back_class = 'navbar navbar-expand-lg navbar-dark fixed-top';
   back_oscuro = 'back-oscuro';
   back_transparente = 'back-transparent';
   back_final = '';
+  loading: boolean = false;
 
   ngOnInit(): void {
+    this.loading = true;
+    setTimeout(()=>{
+      this.loading=false;
+  }, 3000);
     this.back_final = this.back_class + ' ' + this.back_transparente;
   }
 
--- src/app/components/loading/loading.component.ts
+++ src/app/components/loading/loading.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-loading',
   templateUrl: './loading.component.html',
   styleUrls: ['./loading.component.css']
 })
-export class LoadingComponent implements OnInit {
+export class LoadingComponent implements OnInit, AfterViewInit {
 
+  @Input() loading: boolean = false;
   constructor() { }
 
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
+    this.loading = false;
   }
 
+  ngOnInit(): void {
+    this.loading = true;
+    setTimeout(() => {
+      document.querySelector('.img').classList.toggle('active');
+    }, 500)
+  }
 }
